4ff05c0dc15b062b10dfeb3568d6f0cbb01d5256
Sveta Miasota <pophopka@gmail.com>
2025-01-16 03:17:38 +0300

    added functionality and styles for slides

--- src/slides/slides.tsx
+++ src/slides/slides.tsx
@@ -1,32 +1,83 @@
 import "./slides.scss";
-import Swiper from "swiper";
+import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
+import {Parallax} from "swiper/modules";
 
 function Slides() {
 
-    const swiper = new Swiper('.swiper', {
-        freeMode: true, 
-        centeredSlides: true,
-        // breakpoints: {
-        //     0: {
-        //         slidesPerView: 2.5,
-        //     }
-        // }
-    })
+    // const swiper = new Swiper('.swiper', {
+    //     freeMode: true, 
+    //     centeredSlides: true,
+    //     mousewheel: true,
+    //     parallax: true,
+    //     breakpoints: {
+    //         0: {
+    //             slidesPerView: 2.5,
+    //             spaceBetween: 20
+    //         },
+    //         680: {
+    //             slidesPerView: 3.5, 
+    //             spaceBetween: 60
+    //         }
+    //     }
+    // })
 
     return (
         //Slider main container
-        <div className="swiper"> 
+        <Swiper 
+            freeMode={true}
+            centeredSlides={true}
+            mousewheel={true}
+            parallax={true}
+            breakpoints={{
+                0: {
+                slidesPerView: 2.5,
+                spaceBetween: 20
+            },
+            680: {
+                slidesPerView: 3.5, 
+                spaceBetween: 60
+            }
+            }}
+            modules={[Parallax]}
+            className="swiper"    > 
             {/* Additional required wrapper */}           
             <div className="swiper-wrapper">
                 {/*Slides*/}
-                <div className="swiper-slide">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla deserunt, perspiciatis dolore corrupti nam eos eligendi expedita aliquam ipsum error ullam iste corporis commodi minus exercitationem deleniti optio tempora veniam.</div>
-                <div className="swiper-slide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non id iusto placeat culpa modi omnis sed aperiam quo doloremque, eius obcaecati a. Quo quam deserunt eaque ea labore, mollitia soluta.</div>
-                <div className="swiper-slide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam accusantium architecto ullam! Delectus vero omnis voluptatibus laudantium laborum! Non minus fugiat eum, quisquam modi voluptatibus aspernatur odio nesciunt suscipit quam.</div>
-                <div className="swiper-slide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolores maiores esse laborum, velit consequatur, quia doloremque dolorem voluptatibus, dolore illum excepturi amet maxime fugit nisi accusamus eum nam molestias.</div>
-                <div className="swiper-slide">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet nobis laboriosam excepturi sequi harum delectus quae quasi neque! Eum aperiam, animi corrupti quam minima vel culpa nulla accusantium saepe earum?</div>
+                <SwiperSlide>
+                    <div className="slides-item">
+                        <div className="slide-img" data-swiper-parallax="20%" style={{backgroundImage: "url(/images/slides/4.jpg)"}}></div>
+                    </div> 
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slides-item">
+                        <div className="slide-img" data-swiper-parallax="20%" style={{backgroundImage: "url(/images/slides/1.jpg)"}}></div>
+                    </div> 
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slides-item">
+                        <div className="slide-img" data-swiper-parallax="20%" style={{backgroundImage: "url(/images/slides/1.jpg)"}}></div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slides-item">
+                        <div className="slide-img" data-swiper-parallax="20%" style={{backgroundImage: "url(/images/slides/1.jpg)"}}></div>
+                    </div> 
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slides-item">
+                        <div className="slide-img" data-swiper-parallax="20%" style={{backgroundImage: "url(/images/slides/1.jpg)"}}></div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slides-item">
+                        <div className="slide-img" data-swiper-parallax="20%" style={{backgroundImage: "url(/images/slides/1.jpg)"}}></div>
+                    </div> 
+                </SwiperSlide>             
+                 
+                
             </div>
-        </div>
+        </Swiper>
     );
 }
 
